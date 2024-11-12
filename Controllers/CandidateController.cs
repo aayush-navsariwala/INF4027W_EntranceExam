05d1be07188558f8cc6f82a730f68c5f8ce76170
aayush-navsariwala <aayush.nav@gmail.com>
2024-11-12 11:40:51 +0200

    Commit 4 - Added initial controllers to be used

---
+++ Controllers/CandidateController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace INF4001N_1814748_NVSAAY001_2024.Controllers
+{
+    public class CandidateController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
