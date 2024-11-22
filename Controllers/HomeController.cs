86485263a892df480b7248352e5a1f235a49ad78
aayush-navsariwala <aayush.nav@gmail.com>
2024-11-22 15:58:54 +0200

    Commit 29 - Added comments to HomeController

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,26 +6,26 @@ namespace INF4001N_1814748_NVSAAY001_2024.Controllers
 {
     public class HomeController : Controller
     {
+        //Logger instance for logging application events and errors
         private readonly ILogger<HomeController> _logger;
 
+        //Constructor to inject the ILogger service
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
         }
 
+        //Action to render the Home/Index view
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
+        //Action to handle and display error information
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
+            //Creates a new ErrorViewModel with the current request's trace identifier
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
--- obj/Debug/net8.0/INF4001N_1814748_NVSAAY001_2024.AssemblyInfo.cs
+++ obj/Debug/net8.0/INF4001N_1814748_NVSAAY001_2024.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("INF4001N_1814748_NVSAAY001_2024")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b004f9759fe3f4697145ceaa044c85a25c0619a9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5016e9535391f7a69d37de128a40071c74e9c247")]
 [assembly: System.Reflection.AssemblyProductAttribute("INF4001N_1814748_NVSAAY001_2024")]
 [assembly: System.Reflection.AssemblyTitleAttribute("INF4001N_1814748_NVSAAY001_2024")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
